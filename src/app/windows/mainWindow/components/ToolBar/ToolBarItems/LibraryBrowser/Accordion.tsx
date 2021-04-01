14656718c566287a4516f8b0c1bc5b4a30ef5c25
Aidan Petti <apetti1920@aol.com>
2021-04-01 11:02:39 -0400

    0.4.2d
    - Began implimenting accordion feature within the LibraryBrowser

--- src/app/windows/mainWindow/components/ToolBar/ToolBarItems/LibraryBrowser/Accordion.tsx
+++ src/app/windows/mainWindow/components/ToolBar/ToolBarItems/LibraryBrowser/Accordion.tsx
@@ -1,11 +1,24 @@
 // @flow
 import * as React from 'react';
+import _ from "lodash";
+
+export type AccordionDataType = {
+    id: number,
+    name: string,
+    keywords: string[]
+}
+
+const accordionStyle: React.CSSProperties = {
+    width: "100%",
+    backgroundColor: "blue",
+    paddingTop: "5px"
+}
 
 type Props = {
-    cards: React.ReactElement[]
+    data: AccordionDataType[]
 };
 type State = {
-    collapsed: boolean
+    expandedSubsectionID?: string
 };
 
 export class Accordion extends React.Component<Props, State> {
@@ -13,22 +26,63 @@ export class Accordion extends React.Component<Props, State> {
         super(props);
 
         this.state = {
-            collapsed: true
+            expandedSubsectionID: undefined
         }
     }
 
-    getAccordionStyle = (): React.CSSProperties => ({
-        width: "100%",
-        height: "30px",
-        margin: "5px",
-        backgroundColor: "blue"
-    })
+    getOrderedKeywordList = (): string[] => {
+        const allKeywords = this.props.data.map(d => d.keywords).reduce((a, v) => a.concat(v));
+        const uniqueKeywords = [...new Set(allKeywords)];
+        uniqueKeywords.sort((a, b): number => {
+            const diff = this.props.data.filter(d => d.keywords.includes(b)).length -
+                this.props.data.filter(d => d.keywords.includes(a)).length;
+            if (diff!==0) { return diff }
+            else { return b<a?1:-1 }
+        });
+        return uniqueKeywords
+    }
 
     render(): React.ReactNode {
-        return (
-            <div style={this.getAccordionStyle()}>
+        const keywordSeparator = (keyword: string) => (
+            <div style={{width: "100%", height: "15px", backgroundColor: "green", marginBottom: "5px", cursor: "pointer"}}
+                 onClick={()=>{
+                     const tempState: State = _.cloneDeep(this.state);
+                     if (tempState.expandedSubsectionID===undefined) {
+                         tempState.expandedSubsectionID = keyword;
+                     } else {
+                         tempState.expandedSubsectionID = undefined
+                     }
+                     this.setState(tempState);
+                 }}>{keyword}</div>
+        )
 
-            </div>
+        const unExpandedElement = (
+            this.getOrderedKeywordList().map(k => (
+                React.cloneElement(keywordSeparator(k), {key: k})
+            ))
+        );
+
+        const expandedElement = (
+            <React.Fragment>
+                {keywordSeparator(this.state.expandedSubsectionID)}
+                <div style={{width: "100%", backgroundColor: "teal", display: "flex", flexFlow: "row wrap"}}>
+                    {
+                        this.props.data.filter(d => d.keywords.includes(this.state.expandedSubsectionID)).map(d => (
+                            <div key={d.id}
+                                 style={{width: "25px", height: "25px", backgroundColor: "black", margin: "5px", color: "white"}}>{d.name}</div>
+                        ))
+                    }
+                </div>
+            </React.Fragment>
+        )
+
+        return (
+            <React.Fragment>
+                <div style={{width: "100%", height: "30px", backgroundColor: "red"}}/>
+                <div style={accordionStyle}>
+                    {this.state.expandedSubsectionID===undefined?unExpandedElement:expandedElement}
+                </div>
+            </React.Fragment>
         );
     }
 }
--- src/app/windows/mainWindow/components/ToolBar/ToolBarItems/LibraryBrowser/LibraryBrowser.tsx
+++ src/app/windows/mainWindow/components/ToolBar/ToolBarItems/LibraryBrowser/LibraryBrowser.tsx
@@ -4,6 +4,7 @@ import * as React from 'react';
 import Portal from "../../../ComponentUtils/Portal"
 import {PointType} from "../../../../../../../shared/types";
 import theme, {GetGlassStyle} from "../../../../../../theme";
+import {Accordion} from "./Accordion";
 
 type Props = {
     open: boolean
@@ -17,7 +18,7 @@ type State = {
 export class LibraryBrowser extends React.Component<Props, State> {
     GetLibraryBrowserStyle = (): React.CSSProperties => ({
         width: "300px",
-        height: "600px",
+        maxHeight: "600px",
         position: 'fixed',
         top: `${this.props.location.y}px`,
         left: `${this.props.location.x}px`,
@@ -25,13 +26,22 @@ export class LibraryBrowser extends React.Component<Props, State> {
         pointerEvents: this.props.open ? "auto" : "none",
         display: "inline-block",
         opacity: this.props.open ? 1 : 0,
+        padding: "5px",
         ...GetGlassStyle(theme.palette.accent, 0.3)
     })
 
     render(): React.ReactNode {
         return (
             <Portal>
-                <div style={this.GetLibraryBrowserStyle()}/>
+                <div style={this.GetLibraryBrowserStyle()}>
+                    <Accordion data={
+                        [
+                            {id: 1, name: "Sum", keywords: ["Math", "Frequently Used"]},
+                            {id: 2, name: "Integral", keywords: ["Math", "DSP"]},
+                            {id: 3, name: "Gain", keywords: ["Math", "Frequently Used", "Temp"]}
+                        ]
+                    }/>
+                </div>
             </Portal>
         );
     }
